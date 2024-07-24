a60dfc7c1614947808bb5a52485f8c37548f997b
Umesh Kumar <103483751+Umeshboby@users.noreply.github.com>
2024-07-24 12:15:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+    const colorSelect = document.getElementById('colorSelect');
+    const selectedIndex = colorSelect.selectedIndex;
+
+    if (selectedIndex !== -1) {
+        colorSelect.remove(selectedIndex);
+    }
+}
